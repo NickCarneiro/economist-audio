5759a25ba23c3394299dc069e97469df8f73faa9
Nick Carneiro <burthawk101@gmail.com>
2013-12-26 21:39:45 -0600

    added abandonment message

--- extract.py
+++ extract.py
@@ -1,42 +1,16 @@
-from pyquery import PyQuery
+from ghost import Ghost
 from local import settings
-import requests
 
-print 'getting form_build_id'
-# first we need to get a form_build_id from the login form html
-r = requests.get('http://www.economist.com/audio-edition/latest')
-cookies = r.cookies
-html = r.text
-q = PyQuery(html)
-inputs = q('input[name="form_build_id"]')
-form_build_id_input = inputs[0]
-form_build_id = q(form_build_id_input).val()
-print 'found it: {}'.format(form_build_id)
-payload = {
-    'name': settings['email_address'],
-    'pass': settings['password'],
-    'form_build_id': form_build_id,
-    'form_id': 'user_login',
-    'securelogin_original_baseurl': 'http://www.economist.com',
-    'op': 'Log in'
-}
-headers = {
-    'content-type': 'application/x-www-form-urlencoded'
-}
+ghost = Ghost()
+page, resources = ghost.open('http://www.economist.com/audio-edition/latest')
+
+result, resources = ghost.fill("#user-login", {
+    "name": settings['email_address'],
+    "pass": settings['password']
+})
 print 'logging in'
-r = requests.post('https://www.economist.com/audio-edition/latest',
-                  cookies=cookies, data=payload, headers=headers)
-html = r.text
-print html
-q = PyQuery(html)
-download_span = q('.audio-issue-full-download-link')
-if len(download_span) == 0:
-    print 'failed to log in'
-    exit(1)
-else:
-    #otherwise, we've got a download link.
-    # get the link that's inside the span
-    links = q(download_span).children()
-    link = links[0]
-    href = q(link).attr('href')
-    print 'download link {}'.format(href)
+page, resources = ghost.fire_on("#user-login", "submit", expect_loading=True)
+download_url, resources = ghost.evaluate(
+    "document.querySelector('.audio-issue-full-download-link > a').getAttribute('href');")
+
+print 'download url {}'.format(download_url)
--- readme.md
+++ readme.md
@@ -1,5 +1,13 @@
 # Download The Economist in Audio
 
+A python script to download the economist mp3s to your pc.
+
+# Warning
+
+This doesn't actually work. The login fails. I believe it's because there are a bunch of javascript cookies that get included with the login post request.
+
+I attempted to use Ghost.py which looks really promising, but the dependencies don't work out of the box on OSX so I'm abandoning this for now.
+
 # Setup
 
 * make a virtualenv if you want
